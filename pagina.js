5de149e8623483cf1199c80ff4a4db1d27fa61de
JordaneaStatute <joyjoy_statute2008@hotmail.com>
2023-07-05 11:45:47 -0300

    paginadinamnica

---
+++ pagina.js
@@ -0,0 +1,12 @@
+    window.addEventListener('load', () => {
+   
+        const h1 = document.createElement('h1');
+        const h2 = document.createElement('h2');
+        const p1 = document.createElement('p1');
+
+
+        document.body.appendChild(h1).innerHTML = 'Página dinamica';
+        document.body.appendChild(h2).innerHTML = 'Dinamico';
+        document.body.appendChild(p1).innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, eligendi provident! Dignissimos sequi veniam neque quae inventore sapiente non dolorem. Accusamus, aliquam adipisci omnis corrupti provident autem ipsa velit quae';
+
+    });
