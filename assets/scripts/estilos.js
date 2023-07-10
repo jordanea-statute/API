2338c3b1da8d960e48c1feed3399c9d5c2aeb949
JordaneaStatute <joyjoy_statute2008@hotmail.com>
2023-07-10 10:34:35 -0300

    fonts estilo.js

--- assets/scripts/estilos.js
+++ assets/scripts/estilos.js
@@ -1,7 +1,36 @@
-const styles = document.createElement('link'); /* SERVE PARA LINkAR TODAS AS PAGINAS SEM PRECISAR POR LINK  rel stylesheet EM TODAS AS PAGINAS. */
 
+const reset = document.createElement('link');
+reset.rel = 'StyleSheet';
+reset.href = './assets/style/reset.css';
+
+
+const styles = document.createElement('link'); /* SERVE PARA LINkAR TODAS AS PAGINAS SEM PRECISAR POR LINK  rel stylesheet EM TODAS AS PAGINAS. */
 styles.setAttribute('rel', 'StyleSheet');
 styles.setAttribute('href', './assets/styles/style.css');
 
-document.head.appendChild(styles);
+
+    const linkGoogleFontes = document.createElement('Link');
+    linkGoogleFontes.rel = 'preconnect';
+    linkGoogleFontes.href = 'https://fonts.googleapis.com';
+
+    const continuacaoFontStatic = document.createElement('Link');
+    continuacaoFontStatic.rel = 'preconnect';
+    continuacaoFontStatic.href = 'https://fonts.gstatic.com';
+
+        const terceiraParteFontRoboto = document.createElement('Link');
+        terceiraParteFontRoboto.rel = 'stylesheet';
+        terceiraParteFontRoboto.href = 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,200;0,400;1,200;1,400&display=swap';
+
+        const terceiraPartOutraLetraIndie = document.createElement('link');
+        terceiraPartOutraLetraIndie.rel = 'stylesheet';
+        terceiraPartOutraLetraIndie.href = 'https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap';
+
+        document.head.append(reset, linkGoogleFontes, continuacaoFontStatic, terceiraParteFontRoboto, terceiraPartOutraLetraIndie, styles);
+      
+
+
+
+
+
+
 
