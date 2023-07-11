9cd32ff2140d59d92287e57a45969c5613d2dba6
JordaneaStatute <joyjoy_statute2008@hotmail.com>
2023-07-11 12:00:54 -0300

    estilização put

--- assets/scripts/estilos.js
+++ assets/scripts/estilos.js
@@ -1,29 +1,35 @@
 
 const reset = document.createElement('link');
-reset.rel = 'StyleSheet';
-reset.href = './assets/style/reset.css';
+reset.rel = 'styleSheet';
+reset.href = './assets/styles/reset.css';
 
 
 const styles = document.createElement('link'); /* SERVE PARA LINkAR TODAS AS PAGINAS SEM PRECISAR POR LINK  rel stylesheet EM TODAS AS PAGINAS. */
-styles.setAttribute('rel', 'StyleSheet');
+styles.setAttribute('rel', 'styleSheet');
 styles.setAttribute('href', './assets/styles/style.css');
 
 
-    const linkGoogleFontes = document.createElement('Link');
+    const linkGoogleFontes = document.createElement('link');
     linkGoogleFontes.rel = 'preconnect';
     linkGoogleFontes.href = 'https://fonts.googleapis.com';
 
-    const continuacaoFontStatic = document.createElement('Link');
+    const continuacaoFontStatic = document.createElement('link');
     continuacaoFontStatic.rel = 'preconnect';
     continuacaoFontStatic.href = 'https://fonts.gstatic.com';
+    continuacaoFontStatic.crossorigin = '';
 
-        const terceiraParteFontRoboto = document.createElement('Link');
+        const terceiraParteFontRoboto = document.createElement('link');
         terceiraParteFontRoboto.rel = 'stylesheet';
         terceiraParteFontRoboto.href = 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,200;0,400;1,200;1,400&display=swap';
 
         const terceiraPartOutraLetraIndie = document.createElement('link');
         terceiraPartOutraLetraIndie.rel = 'stylesheet';
         terceiraPartOutraLetraIndie.href = 'https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap';
+
+        const terceiraPartOutraLetraUbuntu = document.createElement('link');
+        terceiraPartOutraLetraUbuntu.rel = 'stylesheet';
+        terceiraPartOutraLetraUbuntu.href = 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap';
+       
 
         document.head.append(reset, linkGoogleFontes, continuacaoFontStatic, terceiraParteFontRoboto, terceiraPartOutraLetraIndie, styles);
       
--- assets/scripts/navBar.js
+++ assets/scripts/navBar.js
@@ -1,20 +1,26 @@
 
 const home = document.createElement('a');
 home.href = './index.html';
+// home.innerHTML = 'Home';
 
 const creatPost =document.createElement('a');
 creatPost.href = './create-com-post.html';
+// creatPost.innerHTML = 'Post';
 
 const readGet = document.createElement('a');
 readGet.href = './read-com-get.html';
+// readGet.innerHTML = 'Get';
 
 const updatePut = document.createElement('a');
 updatePut.href = './update-com-put.html';
+// updatePut.innerHTML = 'Put';
 
 const deleteDel = document.createElement('a');
 deleteDel.href = './delete-com-delete.html';
+// deleteDel.innerHTML = 'Delete';
 
 const pageDinamica = document.createElement('a');
 pageDinamica.href = './pagina-dinamica.html';
+// pageDinamica.innerHTML = 'PageDinamica';
 
-document.head.append(home, creatPost, readGet, updatePut, deleteDel, pageDinamica);
+document.querySelector('header').append(home, creatPost, readGet, updatePut, deleteDel, pageDinamica);
