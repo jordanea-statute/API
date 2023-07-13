0df49bc787bc328ec8016047651b98766d72710a
JordaneaStatute <joyjoy_statute2008@hotmail.com>
2023-07-13 09:10:00 -0300

    alinhamento botao

--- assets/scripts/estilos.js
+++ assets/scripts/estilos.js
@@ -31,7 +31,7 @@ styles.setAttribute('href', './assets/styles/style.css');
         terceiraPartOutraLetraUbuntu.href = 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap';
        
 
-        document.head.append(reset, linkGoogleFontes, continuacaoFontStatic, terceiraParteFontRoboto, terceiraPartOutraLetraIndie, styles);
+        document.head.append(reset, linkGoogleFontes, continuacaoFontStatic, terceiraParteFontRoboto, terceiraPartOutraLetraIndie, terceiraPartOutraLetraUbuntu, styles);
       
 
 
